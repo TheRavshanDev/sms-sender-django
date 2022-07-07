26aea1cfaf3fb4e5f1d522a5fdcf1e2fa88d17f7
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-07-07 11:43:50 +0500

    Added middleware.py file

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'base.middleware.TestMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
